19e1f6e4de7e02789977cae089e19807531b7839
isalias98 <isa_lias@yahoo.com>
2024-05-14 14:17:49 +0800

    Astro Config update

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,6 +7,6 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://example.com',
+  site: 'https://isalias98.github.io',
   integrations: [mdx(), sitemap(), react(), tailwind()]
 });
